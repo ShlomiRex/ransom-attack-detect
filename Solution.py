2653fc434a6164f0827e0ae32fe40a2bbb9e521e
Shlomi <vgtvgy1@gmail.com>
2019-05-08 17:34:26 +0300

    Currently it is working

--- Solution.py
+++ Solution.py
@@ -1,9 +1,10 @@
 import difflib # For file diff
 
-# For watching files (created, deleted...)
+
 import sys
 import time
 import logging
+# For watching files (created, deleted...)
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 from watchdog.events import FileSystemEventHandler
@@ -23,14 +24,21 @@ import re
 import string
 
 
-## Global Variables ##
-flag = 0
+#### Global Variables ####
+flag = 0 # File modification raises 2 event handlers, so use flag to only 1.
 is_windows = True if os.name == 'nt' else False
 if len(sys.argv) < 2:
 	raise Exception("Invalid directory watch path")
 	exit()
 watch_dir = sys.argv[1]
+
+
+## Backup Variables ##
 backup_dir = ""
+
+## Ransomware Hyperparameter ##
+words_added = 0.5 # If this amount of words are ADDED to the file(by %), alert
+
 
 
 def wordsAndFreq(file):
@@ -45,15 +53,20 @@ def wordsAndFreq(file):
 		 
 	frequency_list = frequency.keys()
 	 
-	for words in frequency_list:
-		print(words, frequency[words])
-
+	#for words in frequency_list:
+		#print(words, frequency[words])
+		
+	return frequency
+	
+	
 def backupFile(file):
+	file = os.path.abspath(file)
+	print(f"Backing {file} to {backup_dir}")
 	global is_windows
 	if is_windows == True:
-		shutil.copyfile(file, backup_dir+"\\")
+		shutil.copy(file, backup_dir)
 	else:
-		shutil.copyfile(file, backup_dir+"/")
+		shutil.copy(file, backup_dir)
 
 # Called when a file is modified (The body of the detector)
 def fileModification(path):
@@ -65,13 +78,42 @@ def fileModification(path):
 		return
 	print(f"File modified: {full_path}")
 	
-	wordsAndFreq(full_path)
+	frequency = wordsAndFreq(full_path)
+	words_count = 0
+	for word in frequency.keys():
+		words_count += frequency[word]
+	
+	backup_words_count = 0
+	print(f"Path = {path}")
+	if is_windows == True:
+		backup_frequency = wordsAndFreq(backup_dir+"\\"+path)
+	else:
+		backup_frequency = wordsAndFreq(backup_dir+"/"+path)
+	
+	for word in backup_frequency.keys():
+		backup_words_count += backup_frequency[word]
+		
+	print(f"Current words: {words_count} , But before that it was: {backup_words_count}")
+	delta_words_count = words_count - backup_words_count
+	print(f"Delta count = {delta_words_count}")
+	
+	# If removed words, do nothing, no encryption will do that
+	if delta_words_count > 0:
+		p = delta_words_count/words_count
+		print(f"Precentage # of words added: {p}")
+		if p > words_added:
+			print("ALERT! RANSOMWARE DETECTED!")
+			exit()
+	
+	backupFile(full_path)
 	
 
 	
 class MyEventHandler(FileSystemEventHandler):
 	def on_modified(self, event):
-		_thread.start_new_thread(fileModification, (event.src_path,))
+		# When backing up files, the new thread calls a new event handler which is not good... so, no thread
+		#_thread.start_new_thread(fileModification, (event.src_path,))
+		fileModification(event.src_path)
 		
 		
 def startWatching():
@@ -113,8 +155,10 @@ def startBackup():
 	else:
 		copytree(cur_dir, backup_dir+"/")
 	
-
+def initHyperparameters():
+	pass
 
 if __name__ == "__main__":
 	startBackup()
+	initHyperparameters()
 	startWatching()
